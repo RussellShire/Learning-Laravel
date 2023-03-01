31c9c774769f55768b6f396030e4c780af1a03ae
Russell Shire <russell.shire@gmail.com>
2023-03-01 11:17:41 +0000

    Add simplifed Route code and comments for future reference

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,11 +18,8 @@ Route::get('/', function () {
     return view('posts');
 });
 
-Route::get('posts/{post}', function ($slug) { // {post} here is a wildcard that can be passed into the function
-//    Find a post by it\'s value and pass it to the view called "post"
-    $post = Post::find($slug); // post = Post class, find = method on the Post class (this moves the logic from the controller (here) to the Model).
-
-    return view('post', [ // sending the contents of the post file to the view called post, to be rendered
-        'post' => $post
+Route::get('posts/{post}', function ($slug) {        // {post} here is a wildcard that can be passed into the function as the argument $slug
+    return view('post', [                           // sending the contents of the post file to the view called post, to be rendered
+        'post' => Post::find($slug)                      // post = Post class, find = method on the Post class (this moves the logic from the Controller (here) to the Model).
     ]);
-})->where('post', '[A-z_\-]+'); // performing regex on our wildcard (defined after 'get') to stop crazy things being passed in;
+})->where('post', '[A-z_\-]+');           // performing regex on our wildcard (defined after 'get') to stop crazy things being passed in;
