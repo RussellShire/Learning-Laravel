4c736a13ca1321698317677a47042fddc910d871
Russell Shire <russell.shire@gmail.com>
2023-03-01 16:34:51 +0000

    Add class for Post

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,7 @@
 
 use \App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use \Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+        $files = File::files(resource_path("posts/"));
+        $posts = [];
+
+        foreach ($files as $file) {
+            $document = YamlFrontMatter::parseFile($file);
+
+            $posts[] = new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        }
+
+        var_dump($posts[0]->title);
+
     return view('posts', [
-        'posts' => Post::all()
+       'posts' => $posts
     ]);
 });
 
