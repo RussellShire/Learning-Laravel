8619b920f17e58c9ed6c933b568d7596e7527a86
Russell Shire <russell.shire@gmail.com>
2023-03-09 10:34:49 +0000

    Add Route Model Binding to routes for Post class

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+//    protected $guarded = ['id']; //
+
+    protected $fillable = ['title', 'excerpt', 'body']; // Allows mass assigning posts with Post::create(['title' => "my third post", etc etc]), array allows what can be mass assigned
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -21,8 +21,16 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {        // {post} here is a wildcard that can be passed into the function as the argument $id
-    return view('post', [                           // sending the contents of the post file to the view called post, to be rendered
-        'post' => Post::findOrFail($id)                      // post = Post class, find = method on the Post class (this moves the logic from the Controller (here) to the Model).
+// Route Model binding
+Route::get('posts/{post}', function (Post $post) {        // {post} here is a wildcard that gets passed into the function
+    return view('post', [                               // sending the contents of the post file to the view called post, to be rendered
+        'post' => $post                                     // $post = id of the Post Model
     ]);
 });
+
+// Manually assigning everything
+//Route::get('posts/{post}', function ($id) {          // {post} here is a wildcard that can be passed into the function as the argument $id
+//    return view('post', [                           // sending the contents of the post file to the view called post, to be rendered
+//        'post' => Post::findOrFail($id)            // post = Post class, find = method on the Post class (this moves the logic from the Controller (here) to the Model).
+//    ]);
+//});
