2f8c284d0482419e51f261e501509204e9e97f4c
Russell Shire <russell.shire@gmail.com>
2023-03-13 17:00:30 +0000

    Add refactoring for more complicated CSS and layout

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,35 +1,52 @@
 <x-layout> {{-- using the blade component layout.blade from components folder --}}
-{{--  Checking for homepage and loading appropriate title  --}}
-    @if($isHompage = Request::is('/'))
-        <h1>My Blog</h1>
-    @else
-        <h1>{{ ucfirst(Request::segments()[0]) }}</h1>
-    @endif
+    @include ('_posts-header')
 
-    {{-- posts is an array of all post files as objects, we loop over and build the homepage --}}
-    @foreach ($posts as $post) {{-- Laravel syntax foreach --}}
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}" > {{-- accessing a value from the Post Class object --}}
-                    {{ $post->title }} {{-- blade.php laravel syntax replaces vannilla php "<?= $post->title; ?>" --}}
-                </a>
-            </h1>
-            <x-display-post-attributes
-                :authorSlug="$post->author->username"
-                :author="$post->author->name"
-                :categorySlug="$post->category->slug"
-                :category="$post->category->name"
-            />
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+
+    {{--  Checking for homepage and loading appropriate title  --}}
+{{--    @if($isHompage = Request::is('/'))--}}
+{{--        <h1>My Blog</h1>--}}
+{{--    @else--}}
+{{--        <h1>{{ ucfirst(Request::segments()[0]) }}</h1>--}}
+{{--    @endif--}}
+
+{{--    --}}{{-- posts is an array of all post files as objects, we loop over and build the homepage --}}
+{{--    @foreach ($posts as $post) --}}{{-- Laravel syntax foreach --}}
+{{--        <article>--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{ $post->slug }}" > --}}{{-- accessing a value from the Post Class object --}}
+{{--                    {{ $post->title }} --}}{{-- blade.php laravel syntax replaces vannilla php "<?= $post->title; ?>" --}}
+{{--                </a>--}}
+{{--            </h1>--}}
+{{--            <x-display-post-attributes--}}
+{{--                :authorSlug="$post->author->username"--}}
+{{--                :author="$post->author->name"--}}
+{{--                :categorySlug="$post->category->slug"--}}
+{{--                :category="$post->category->name"--}}
+{{--            />--}}
+{{--            <div>--}}
+{{--                {{ $post->excerpt }}--}}
+{{--            </div>--}}
+{{--        </article>--}}
+{{--    @endforeach--}}
 
 {{--  Loading back button on none-homepage  --}}
-    @if(!$isHompage)
-        <p>
-            <a href="/">Homepage</a>
-        </p>
-    @endif
+{{--    @if(!$isHompage)--}}
+{{--        <p>--}}
+{{--            <a href="/">Homepage</a>--}}
+{{--        </p>--}}
+{{--    @endif--}}
 </x-layout>
