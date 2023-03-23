4a5b3fde1cfd72645fb43e0cd59009801bacca31
Russell Shire <russell.shire@gmail.com>
2023-03-23 17:01:36 +0000

    Add a very basic route and view

--- jobs-site/routes/web.php
+++ jobs-site/routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus quam id ante congue, at commodo sem bibendum. Fusce eu nulla eget neque sagittis hendrerit sit amet eu turpis. Proin venenatis ut nisi vel molestie. Proin vitae ultricies neque. Etiam convallis at justo at sodales. Maecenas vitae diam eget nibh eleifend faucibus in eu tellus. Suspendisse tincidunt metus semper nibh euismod, sit amet viverra tortor venenatis. Curabitur lacus justo, aliquet ac gravida non, bibendum vehicula est. Mauris commodo nisl ultricies, ullamcorper tortor vitae, consectetur ante. Curabitur vel ante ac orci rhoncus fringilla. Pellentesque venenatis nisl eu lectus commodo fringilla. Sed velit neque, tincidunt id turpis id, dictum dapibus libero. Morbi congue vulputate nisi. Morbi et dui vitae erat auctor viverra. Nullam ut ultricies ipsum.',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus quam id ante congue, at commodo sem bibendum. Fusce eu nulla eget neque sagittis hendrerit sit amet eu turpis. Proin venenatis ut nisi vel molestie. Proin vitae ultricies neque. Etiam convallis at justo at sodales. Maecenas vitae diam eget nibh eleifend faucibus in eu tellus. Suspendisse tincidunt metus semper nibh euismod, sit amet viverra tortor venenatis. Curabitur lacus justo, aliquet ac gravida non, bibendum vehicula est. Mauris commodo nisl ultricies, ullamcorper tortor vitae, consectetur ante. Curabitur vel ante ac orci rhoncus fringilla. Pellentesque venenatis nisl eu lectus commodo fringilla. Sed velit neque, tincidunt id turpis id, dictum dapibus libero. Morbi congue vulputate nisi. Morbi et dui vitae erat auctor viverra. Nullam ut ultricies ipsum.',
+            ]
+        ]
+
+    ]);
 });
