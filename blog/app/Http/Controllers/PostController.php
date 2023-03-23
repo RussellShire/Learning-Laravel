4b68dcfdf4c0bb6867dd3bb094f919375e04db6e
Russell Shire <russell.shire@gmail.com>
2023-03-23 11:43:47 +0000

    Add simple pagination to index pages

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -11,14 +11,16 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),               // filter() is a scoped query set on the Post Model
+            'posts' => Post::latest()->filter(
+                request(['search', 'category', 'author']) // filter() is a scoped query set on the Post Model
+            )->paginate(6),                               // using 'paginate' instead of 'get'
             ]);
     }
 
     public function show(Post $post)
     {
-        return view('posts.show', [   // sending the contents of the post file to the view called post, to be rendered
-            'post' => $post,        // $post = id of the Post Model
+        return view('posts.show', [     // sending the contents of the post file to the view called post, to be rendered
+            'post' => $post,                // $post = id of the Post Model
         ]);
     }
 }
--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -10,10 +10,13 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count()) {{-- Checking for posts and loading message if none --}}
             <x-posts-grid :posts="$posts" />
+
+            {{ $posts->links() }} {{-- Render pagination links --}}
         @else
             <p class="text-center">No posts yet, please check back later.</p>
         @endif
     </main>
+
 
     {{-- Loading back button on non-homepage pages --}}
     @if(!$isHompage)
