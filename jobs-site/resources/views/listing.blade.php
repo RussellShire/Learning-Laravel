a80c074b6ac2a68d4444c445971b9c94dc0f2ea5
Russell Shire <russell.shire@gmail.com>
2023-03-24 16:04:12 +0000

    Add basic layout blade template

--- jobs-site/resources/views/listing.blade.php
+++ jobs-site/resources/views/listing.blade.php
@@ -1,10 +1,14 @@
-<h2>
-    {{ $listing['title'] }}
-</h2>
-<p>
-    {{ $listing['description'] }}
-</p>
+@extends('layout')
 
-<button onclick="location.href='/'" type="button">
-    Back
-</button>
+@section('content')
+    <h2>
+        {{ $listing['title'] }}
+    </h2>
+    <p>
+        {{ $listing['description'] }}
+    </p>
+
+    <button onclick="location.href='/'" type="button">
+        Back
+    </button>
+@endsection
--- jobs-site/resources/views/listings.blade.php
+++ jobs-site/resources/views/listings.blade.php
@@ -1,16 +1,20 @@
-<h1>{{ $heading }}</h1>
+@extends('layout')
 
-@if(count($listings) == 0)
-<p>No listings found, please check back later</p>
-@else
-    @foreach($listings as $listing)
-        <h2>
-            <a href="/listings/{{$listing['id']}}">
-                {{ $listing['title'] }}
-            </a>
-        </h2>
-        <p>
-            {{ $listing['description'] }}
-        </p>
-    @endforeach
-@endif
+@section('content')
+    <h1>{{ $heading }}</h1>
+
+    @if(count($listings) == 0)
+    <p>No listings found, please check back later</p>
+    @else
+        @foreach($listings as $listing)
+            <h2>
+                <a href="/listings/{{$listing['id']}}">
+                    {{ $listing['title'] }}
+                </a>
+            </h2>
+            <p>
+                {{ $listing['description'] }}
+            </p>
+        @endforeach
+    @endif
+@endsection
