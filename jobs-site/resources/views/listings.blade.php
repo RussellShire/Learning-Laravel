7de1b0b714f90a45cb9d5b4ac30ef902095e4726
Russell Shire <russell.shire@gmail.com>
2023-03-24 09:21:13 +0000

    Add guard statement for no listings

--- jobs-site/resources/views/listings.blade.php
+++ jobs-site/resources/views/listings.blade.php
@@ -1,5 +1,14 @@
 <h1>{{ $heading }}</h1>
-@foreach($listings as $listing)
-    <h2>{{ $listing['title'] }}</h2>
-    <p>{{ $listing['description'] }}</p>
-@endforeach
+
+@if(count($listings) == 0)
+<p>No listings found, please check back later</p>
+@else
+    @foreach($listings as $listing)
+        <h2>
+            {{ $listing['title'] }}
+        </h2>
+        <p>
+            {{ $listing['description'] }}
+        </p>
+    @endforeach
+@endif
