9ee69c15cdca97ad4be50d43d153e413cbb6d42b
Russell Shire <russell.shire@gmail.com>
2023-03-29 16:45:37 +0100

    Add header and footer styling

--- jobs-site/resources/views/layout.blade.php
+++ jobs-site/resources/views/layout.blade.php
@@ -4,11 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        laravel: "#ef3b2d",
+                    },
+                },
+            },
+        };
+    </script>
     <title>Laravel Jobs</title>
 </head>
-<body>
-<h1>Laravel Jobs</h1>
+<body class="mb-48">
+<nav class="flex justify-between items-center mb-4">
+    <a href="index.html"
+    ><img class="w-24" src="images/logo.png" alt="" class="logo"
+        /></a>
+    <ul class="flex space-x-6 mr-6 text-lg">
+        <li>
+            <a href="register.html" class="hover:text-laravel">
+                <i class="fa-solid fa-user-plus"></i>
+                Register
+            </a>
+        </li>
+        <li>
+            <a href="login.html" class="hover:text-laravel">
+                <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                Login
+            </a>
+        </li>
+    </ul>
+</nav>
+<main>
     @yield('content')
+</main>
+
+<footer
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+>
+    <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
+
+    <a
+        href="create.html"
+        class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
+    >Post Job</a
+    >
+</footer>
 </body>
 </html>
