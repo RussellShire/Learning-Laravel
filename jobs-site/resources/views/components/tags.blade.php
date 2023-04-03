733931703c217951ca42a31b147838f7bc843e6b
Russell Shire <russell.shire@gmail.com>
2023-04-03 12:45:01 +0100

    Add Model Route binding and listing-card component

---
+++ jobs-site/resources/views/components/tags.blade.php
@@ -0,0 +1,14 @@
+@props(['tags'])
+
+@php
+    $tagsArray = explode(", ", $tags);
+@endphp
+<ul class="flex">
+    @foreach($tagsArray as $tag)
+        <li
+            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+        >
+            <a href="#">{{ $tag }}</a>
+        </li>
+    @endforeach
+</ul>
