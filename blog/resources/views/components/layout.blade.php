10cb8cefb80c2cc89bac0a38fdf076546035f1e1
Russell Shire <russell.shire@gmail.com>
2023-04-18 11:31:47 +0100

    Add apline JS to set timeout on flash message

--- blog/resources/views/components/layout.blade.php
+++ blog/resources/views/components/layout.blade.php
@@ -56,7 +56,15 @@
     </section>
 
     @if (session()->has('success'))
-        <div class="fixed bottom-3 right-3 bg-blue-500 rounded-xl text-white py-2 px-4">
+        <div
+            {{--      Using Alpine JS to set a timer on flash message      --}}
+            x-data="{ show: true }"                         {{-- Setting 'state' --}}
+            x-init="setTimeout(() => show = false, 4000)"   {{-- Using JS to change state after 4 secs --}}
+            x-show="show"                                   {{-- Setting initial state --}}
+            {{--      End of Alpine JS      --}}
+            
+            class="fixed bottom-3 right-3 bg-blue-500 rounded-xl text-white py-2 px-4"
+        >
             <p>{{ session('success') }}</p> {{--      Alternative Syntax 'session()->get('success')'     --}}
         </div>
     @endif
